a57415f1d6bce2a77f06b1e7a5f19d35fabf0a44
jenkins <stas85k@gmail.com>
2017-07-05 00:57:29 +0300

    parameterized tests were used new comments were made

--- tests/ru/apackage/SquareTestPrm1.java
+++ tests/ru/apackage/SquareTestPrm1.java
@@ -6,69 +6,53 @@ import org.junit.runners.Parameterized;
 import ru.socialquantum.testtasks.Square;
 import ru.socialquantum.testtasks.SquareFactory;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import static java.lang.Math.pow;
 import static org.junit.Assert.assertEquals;
+import static org.junit.runners.Parameterized.Parameters;
 
 
 /**
  * @author Stanislav Kuznetsov
  **/
-@RunWith(Parameterized.class)
+@RunWith(value = Parameterized.class)
 public class SquareTestPrm1{
-    @Parameterized.Parameters
-    public static Collection<Object[]> data(){
-           /*create and return a Collection
-             of Objects arrays here.
-             Each element in each array is
-             a parameter to your constructor.
-            */
-        Object m1[] = new Object[]{1.0E-17};
-        Object m2[] = new Object[]{1.0E-16};
-        Object m3[] = new Object[]{0.1};
-        Object m4[] = new Object[]{1.0};
-        Object m5[] = new Object[]{9 + 1.0E-14};
-        Object m6[] = new Object[]{10.0};
-        Object m7[] = new Object[]{10 + 1.0E-14};
-        Object m8[] = new Object[]{1.0};
-        Object m9[] = new Object[]{15.0};
-        Object m10[] = new Object[]{20 - 1.0E-14};
-        Object m11[] = new Object[]{20};
-        Object m12[] = new Object[]{20 + 1.0E-14};
-        Object m13[] = new Object[]{25};
-        Object m14[] = new Object[]{30 - 1.0E-14};
-        Object m15[] = new Object[]{30};
-        Object m16[] = new Object[]{1.0E17};
 
-        Collection<Object[]> data = new ArrayList<Object[]>();
-
-        data.add(m1);data.add(m2);data.add(m3);data.add(m4);
-        data.add(m5);data.add(m6);data.add(m7);data.add(m8);
-        data.add(m9);data.add(m10);data.add(m11);data.add(m12);
-        data.add(m13);data.add(m14);data.add(m15);data.add(m16);
-
-        return data;
-
-    }
     private double a;
     public SquareTestPrm1(double a) {
         this.a = a;
-        //this.b = b;
+    }
+    @Parameters
+    public static Collection<Object[]> data(){
+        Object[][]data = new Object[][]{
+                {1.0E-17},{1.0E-16},{0.1},{1.0},
+                {1 + 1.0E-15},{9 + 1.0E-14},{10.0},{10 + 1.0E-14},
+                {15.0},{20 - 1.0E-14},{20.0},{20 + 1.0E-14},
+                {25.0},{30 - 1.0E-14},{30.0},{1.0E17},
+                {0.0},{-1.0},{-9.0},{-10.0},{-19.0},{-30.0}};
+
+        return Arrays.asList(data);
 
     }
 
-    @Test    //Проверка возврата длины стороны квадрата.
+    /*
+    Checking the function that returns the length of the side of the square.
+    */
+    @Test
     public void test1() {
         Square square = SquareFactory.newSquare(a);
         //assertNotEquals(a, square.sideLength(), 0);
         assertEquals(a, square.sideLength(), 0);
-        System.out.println("t1 sideEx  " + a);
-        System.out.println("t1 sideAc  " + square.sideLength());
+        //System.out.println("t1 sideEx  " + a);
+        //System.out.println("t1 sideAc  " + square.sideLength());
     }
 
-    @Test     //Проверка возврата площади квадрата.
+    /*
+    Checking the function that returns the square of the square.
+    */
+    @Test
     public void test2() {
         Square square = SquareFactory.newSquare(a);
         System.out.println("t2 squareAc  " + square.square());
--- tests/ru/apackage/SquareTestPrm2.java
+++ tests/ru/apackage/SquareTestPrm2.java
@@ -5,69 +5,70 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import ru.socialquantum.testtasks.Square;
 import ru.socialquantum.testtasks.SquareFactory;
-
 import java.util.ArrayList;
 import java.util.Collection;
-
-import static org.junit.Assert.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.runners.Parameterized.Parameters;
 
 /**
  * @author Stanislav Kuznetsov
  **/
-    @RunWith(Parameterized.class)
+    @RunWith(value = Parameterized.class)
     public class SquareTestPrm2 {
-    @Parameterized.Parameters
+        private double a, b;
+
+        public SquareTestPrm2(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Parameters
     public static Collection<Object[]> data() {
-           /*create and return a Collection
-             of Objects arrays here.
-             Each element in each array is
-             a parameter to your constructor.
-            */
-        Object m1[] = new Object[]{10.0,10.0};
-        Object m2[] = new Object[]{20.0,20.0};
-        Object m3[] = new Object[]{30.0,30.0};
-        Object m4[] = new Object[]{100.1,100.1};
-        Object m5[] = new Object[]{0.0,0.0};
-        Object m6[] = new Object[]{-1,1};
-        Object m7[] = new Object[]{-9,9};
-        Object m8[] = new Object[]{-10.0,10.0};
-        Object m9[] = new Object[]{-19.0,19};
-        Object m10[] = new Object[]{-20.0,20};
-        Object m11[] = new Object[]{-25.0,25.0};
-        Object m12[] = new Object[]{-30,30};
-        Object m13[] = new Object[]{1.0E-18,1.0E-17};
-        Object m14[] = new Object[]{1 + 1.0E-16,1 + 1.0E-17};
-        Object m15[] = new Object[]{9 + 1.0E-16,9 + 1.0E-17};
+        /*
+      create and return a Collection of Objects arrays here.
+      Each element in each array is a parameter to the constructor.
+        */
+        Object m0[] = new Object[]{10.0,10.0};
+        Object m1[] = new Object[]{20.0,20.0};
+        Object m2[] = new Object[]{30.0,30.0};
+        Object m3[] = new Object[]{100.1,100.1};
+        Object m4[] = new Object[]{0.0,0.0};
+        Object m5[] = new Object[]{-1,1};
+        Object m6[] = new Object[]{-9,9};
+        Object m7[] = new Object[]{-10.0,10.0};
+        Object m8[] = new Object[]{-19.0,19};
+        Object m9[] = new Object[]{-20.0,20};
+        Object m10[] = new Object[]{-25.0,25.0};
+        Object m11[] = new Object[]{-30,30};
+        Object m12[] = new Object[]{1.0E-18,1.0E-17};
+        Object m13[] = new Object[]{1 + 1.0E-16,1 + 1.0E-17};
+        Object m14[] = new Object[]{9 + 1.0E-16,9 + 1.0E-17};
 
         Collection<Object[]> data = new ArrayList<Object[]>();
 
-        data.add(m1);data.add(m2);data.add(m3);data.add(m4);
-        data.add(m5);data.add(m6);data.add(m7);data.add(m8);
-        data.add(m9);data.add(m10);data.add(m11);data.add(m12);
-        data.add(m13);data.add(m14);data.add(m15);
+        data.add(m0);data.add(m1);data.add(m2);data.add(m3);
+        data.add(m4);data.add(m5);data.add(m6);data.add(m7);
+        data.add(m8);data.add(m9);data.add(m10);data.add(m11);
+        data.add(m12);data.add(m13);data.add(m14);
 
         return data;
     }
 
-    private double a, b;
-
-    public SquareTestPrm2(double a, double b) {
-        this.a = a;
-        this.b = b;
-
-    }
-
-   @Test     //Проверка метода, проверяющего равны ли квадраты по площадям.
-    public void test3() {
-
+    /*
+    Checking the function that returns true
+    if the square of the two squares are equals
+    (false if not)
+    */
+   @Test
+    public void test() {
 
         Square square0 = SquareFactory.newSquare(a);
         Square square1 = SquareFactory.newSquare(b);
-        //assertTrue(square0.equalsSquare(square1));
-        assertFalse(square0.equalsSquare(square1));
-        System.out.println("t3 sqr1  " + square0.square());
-        System.out.println("t3 sqr2  " + square1.square());
-        System.out.println("t3 sqrEqu  " + square0.equalsSquare(square1));
+        assertTrue(square0.equalsSquare(square1));
+        //assertFalse(square0.equalsSquare(square1));
+        System.out.println("sqr1  " + square0.square());
+        System.out.println("sqr2  " + square1.square());
+        System.out.println("sqrEqu  " + square0.equalsSquare(square1));
 
     }
 }
